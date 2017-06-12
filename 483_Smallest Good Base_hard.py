3f242d8b03969bb414d34ff1b2e497ff37fcfd3e
dedekinds <469676727@qq.com>
2017-06-12 16:48:25 +0800

    Add files via upload

--- 483_Smallest Good Base_hard.py
+++ 483_Smallest Good Base_hard.py
@@ -1,7 +1,95 @@
 
+
 '''483. Smallest Good Base 
    2017.6.12
-   似乎用pow对大数会GG，本地测评都各种爆了，尽量用对数吧
+   较大的数用对数，较小的数用指数也不行卧槽
+'''
+import math
+def functiontemp1(a,tar):
+    return math.log((tar*(a-1)+1),a)-1
+
+def functiontemp2(a,k):
+    return (pow(a,k+1)-1)/(a-1)
+
+def check(tar,temp):
+    while(int(tar/temp)>=1):
+        if tar%temp!=1:
+            return False
+        else:
+            tar=int(tar/temp)
+    return True
+    
+def binsearch1(tar,k):#精度版二分a,2<=a<=n-1   对数形式，针对大数
+    eps=0.000001#没想到这个常数会出问题
+    left=2
+    right=tar-1
+    if (functiontemp1(left,tar)-k)**2<=eps:
+        return left
+    if (functiontemp1(right,tar)-k)**2<=eps:
+        return right
+    while(left+1!=right):
+        mid=int(left+(right-left)/2)
+        #print(left,right,'k=',k,'mid=',mid)
+        if (functiontemp1(mid,tar)-k)**2<=eps:
+            return mid
+        if functiontemp1(mid,tar)<k:
+            right=mid
+        if functiontemp1(mid,tar)>k:
+            left=mid
+    return -1
+
+def binsearch2(tar,k):#精度版二分a,2<=a<=n-1  指数形式，针对小数
+    eps=0.000001
+    left=2
+    right=tar-1
+    if (functiontemp2(left,k)-tar)**2<eps:
+        return left
+    if (functiontemp2(right,k)-tar)**2<eps:
+        return right
+    while(left+1!=right):
+        mid=int(left+(right-left)/2)
+        #print(left,right,'k=',k,'mid=',mid)
+        if (functiontemp2(mid,k)-tar)**2<eps:
+            return mid
+        if functiontemp2(mid,k)>tar:
+            right=mid
+        if functiontemp2(mid,k)<tar:
+            left=mid
+    return -1
+
+class Solution(object):
+    def smallestGoodBase(self, n):
+        """
+        :type n: str
+        :rtype: str
+        """
+        #1+a+a2+a3+...ak=n=(a^(k+1)-1)/(a-1)
+        #求约数
+        dis=[]
+        #傻逼了，遍历k才是坠吼的
+        n=int(n)
+        for k in range(1,1+int(math.log(n,2))):
+            tar=n
+            if n>100000000:
+                temp=binsearch1(tar,k)
+            else:
+                temp=binsearch2(tar,k)         
+            if temp!=-1 and check(tar,temp):
+                dis.append(temp)
+        if len(dis)==0:
+            return str(n-1)#如果都不是，肯定是n-1            
+        return str(min(dis))
+
+        
+'''483. Smallest Good Base 
+   2017.6.12
+   对数形式也是不行的，由于对数将误差减少了，eps很难满足要求总是有可恶的样例
+'''
+
+
+'''483. Smallest Good Base 
+   2017.6.12
+   似乎用pow对大数会GG，答案是正确的，虽然本地测评都各种爆了，尽量用对数吧
 '''
 import math
 def functiontemp(a,k):
