4ef1192875a987cc9f6bfc8f03f22a22ce6e2c93
dedekinds <469676727@qq.com>
2017-06-18 16:03:02 +0800

    Add files via upload

--- 575_Distribute Candies_easy.py
+++ 575_Distribute Candies_easy.py
@@ -1,4 +1,23 @@
 '''575. Distribute Candies 
+   2017.6.18
+   228ms
+   beats 35.53%
+'''
+
+
+class Solution(object):
+    def distributeCandies(self, candies):
+        """
+        :type candies: List[int]
+        :rtype: int
+        """
+        if int(len(candies)/2)>len(set(candies)):#直接考虑集合
+            return len(set(candies))
+        else:
+            return int(len(candies)/2)
+
+
+'''575. Distribute Candies 
    2017.5.15
    372ms
    beats 13.61%
@@ -13,7 +32,7 @@ class Solution(object):
         """
         A=sorted(candies)
         num=0
-        for key in itertools.groupby(A):
+        for key in itertools.groupby(A):#强大的迭代器
             num+=1
         if num>=len(candies)/2:
             return len(candies)/2
@@ -27,3 +46,16 @@ class Solution(object):
     def distributeCandies(self, candies):
         return min(len(set(candies)), len(candies) / 2)
         return sum(1 - v % 2 for v in collections.Counter(candies).values())
+
+
+
+
+2.groupby()把迭代器中相邻的重复元素挑出来放在一起：统计出现次数
+import itertools
+>>> for key, group in itertools.groupby('AAABBBCCAAA'):
+...     print(key, list(group))
+...
+A ['A', 'A', 'A']
+B ['B', 'B', 'B']
+C ['C', 'C']
+A ['A', 'A', 'A']
