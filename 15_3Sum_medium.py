f015cc737b37e0edcda6a9753dc20776f37c8a9d
dedekinds <469676727@qq.com>
2017-06-22 16:27:57 +0800

    Add files via upload

--- 15_3Sum_medium.py
+++ 15_3Sum_medium.py
@@ -1,7 +1,39 @@
 '''15. 3Sum 
    2017.6.22
-
+   1692ms
 '''
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        ans=[]
+        nums=sorted(nums)
+        for x in range(len(nums)):#先排序O(nlogn)，然后，遍历每个数，另外用两个
+            if x!=0 and nums[x]==nums[x-1]:#下面的TLE主要是这里，相邻相同的跳过
+                continue
+            temp=nums[x]#指针首尾用tow sum的跌跤办法来夹逼，且要遍历完全，一共是O(n2)
+            #nums.pop(x)
+            left=x+1#不需要从0开始
+            right=len(nums)-1
+            while (left<right):
+                if nums[left]+nums[right]==-temp:
+                    ans.append(sorted([temp,nums[left],nums[right]]))#哈希存？
+                    right-=1
+                    left+=1
+                    while left<right and nums[left]==nums[left-1]:
+                        left+=1#防止相同
+                    while left<right and nums[right]==nums[right+1]:
+                        right-=1
+                if nums[left]+nums[right]>-temp:
+                    right-=1
+                if nums[left]+nums[right]<-temp:
+                    left+=1
+            #nums.insert(x,temp)#似乎两个指针要是碰到
+        return ans
+
+——————————————————————————
 class Solution(object):
     def threeSum(self, nums):
         """
