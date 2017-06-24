e5ec54676d71213c4cedc026e240a248dd15c8b2
dedekinds <469676727@qq.com>
2017-06-24 14:31:39 +0800

    Add files via upload

---
+++ 338_ Counting Bits_mudium.py
@@ -0,0 +1,16 @@
+'''338. Counting Bits 
+   2017.6.24
+   185ms
+'''
+class Solution(object):
+    def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        ans=[0]
+        for x in range(1,num+1):
+            ans+=[ans[x>>1]+(x&1)]
+        return ans
+
+详细情况见自己CSDN博客文章
