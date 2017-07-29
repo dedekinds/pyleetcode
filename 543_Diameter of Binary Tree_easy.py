e675c5642c9cc02c034f00b1bebb673fb86f553a
dedekinds <469676727@qq.com>
2017-07-29 19:59:50 +0800

    Add files via upload

--- 543_Diameter of Binary Tree_easy.py
+++ 543_Diameter of Binary Tree_easy.py
@@ -3,7 +3,31 @@
    2017.7.29
 
 '''
+用层次遍历+计算深度之和（不加证明得使用16.18%，速度显然很慢科科
+def height(tree):
+    if not tree:
+        return 0
+    return max(height(tree.left),height(tree.right))+1
 
+def path(root):
+    return height(root.right)+height(root.left)
+
+
+class Solution(object):
+    def diameterOfBinaryTree(self, root):
+        if not root:
+            return 0
+        Q=[root]
+        ans=0
+        while Q:
+            for tree in Q:
+                ans=max(ans,path(tree))
+                tempQ=[]
+                if tree.left:tempQ.append(tree.left)
+                if tree.right:tempQ.append(tree.right)
+            Q=tempQ
+        return ans
+        
 
 
 
