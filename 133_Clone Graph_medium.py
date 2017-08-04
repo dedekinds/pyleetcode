067bd653742bf5afa0d8bc2e03020eba0492fbf3
dedekinds <469676727@qq.com>
2017-08-04 15:54:17 +0800

    Add files via upload

---
+++ 133_Clone Graph_medium.py
@@ -0,0 +1,77 @@
+'''133. Clone Graph 
+   2017.8.4
+'''
+用栈实现DFS
+
+# Definition for a undirected graph node
+# class UndirectedGraphNode:
+#     def __init__(self, x):
+#         self.label = x
+#         self.neighbors = []
+
+class Solution:
+    # @param node, a undirected graph node
+    # @return a undirected graph node
+    def cloneGraph(self, node):
+        if node is None:
+            return node
+        ans=UndirectedGraphNode(node.label)#创建一个图
+        #DFS
+        ans_dict={node.label:ans}#用来判断是否被访问
+        stack=[node]
+        while stack:
+            top=stack.pop()
+            temp=ans_dict[top.label]#准备用来复制neighbors
+            for x in top.neighbors:
+                if x.label not in ans_dict:#如果没有被访问的话
+                    ans_dict[x.label]=UndirectedGraphNode(x.label)
+                    stack.append(x)
+                temp.neighbors.append(ans_dict[x.label])
+        return ans
+————————————————————————————————
+
+# Definition for a undirected graph node
+# class UndirectedGraphNode:
+#     def __init__(self, x):
+#         self.label = x
+#         self.neighbors = []
+
+class Solution:
+    # @param node, a undirected graph node
+    # @return a undirected graph node
+    def cloneGraph(self, node):
+        if not node:
+            return
+        nodeCopy = UndirectedGraphNode(node.label)
+        dic = {node: nodeCopy}
+        queue = [node]
+        # self.bfs(queue, dic)
+        self.dfs(queue, dic)
+        return nodeCopy
+        
+    def bfs(self, queue, dic):
+        while queue:
+            front = queue.pop()
+            for neighbor in front.neighbors:
+                if neighbor not in dic: 
+                    # neighor not visited
+                    neighborCopy = UndirectedGraphNode(neighbor.label)
+                    dic[neighbor] = neighborCopy
+                    dic[front].neighbors.append(neighborCopy)
+                    queue.insert(0, neighbor)
+                else:
+                    # neighbor visited
+                    dic[front].neighbors.append(dic[neighbor])
+                    
+    def dfs(self, stack, dic):
+        while stack:
+            front = stack.pop()
+            for neighbor in front.neighbors:
+                if neighbor not in dic:
+                    neighborCopy = UndirectedGraphNode(neighbor.label)
+                    dic[neighbor] = neighborCopy
+                    dic[front].neighbors.append(neighborCopy)
+                    stack.append(neighbor)
+                else:
+                    dic[front].neighbors.append(dic[neighbor])
+
