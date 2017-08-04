82f46cec757cc6d026bb795e2bf7ca51fea6bf95
dedekinds <469676727@qq.com>
2017-08-04 17:52:50 +0800

    Add files via upload

--- 133_Clone Graph_medium.py
+++ 133_Clone Graph_medium.py
@@ -1,14 +1,37 @@
 '''133. Clone Graph 
    2017.8.4
 '''
-用栈实现DFS
-
+用队列实现BFS
 # Definition for a undirected graph node
 # class UndirectedGraphNode:
 #     def __init__(self, x):
 #         self.label = x
 #         self.neighbors = []
-
+#用BFS试试
+class Solution:
+    # @param node, a undirected graph node
+    # @return a undirected graph node
+    def cloneGraph(self, node):
+        if node is None:
+            return node
+        ans=UndirectedGraphNode(node.label)
+        ans_dict={node.label:ans}
+        queue=[node]
+        self.bfs(queue,ans_dict)
+        return ans
+    
+    def bfs(self,queue,ans_dict):
+        while queue:
+            top=queue.pop()
+            for x in top.neighbors:
+                if x.label not in ans_dict:
+                    temp=UndirectedGraphNode(x.label)
+                    ans_dict[x.label]=temp
+                    queue.insert(0,x)
+                ans_dict[top.label].neighbors.append(ans_dict[x.label])
+                
+    '''   
+用栈实现DFS
 class Solution:
     # @param node, a undirected graph node
     # @return a undirected graph node
@@ -28,6 +51,8 @@ class Solution:
                     stack.append(x)
                 temp.neighbors.append(ans_dict[x.label])
         return ans
+'''
+
 ————————————————————————————————
 
 # Definition for a undirected graph node
