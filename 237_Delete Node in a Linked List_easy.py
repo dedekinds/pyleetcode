dd2cb5358145f578fb2f717d9aaca2110972df0d
dedekinds <469676727@qq.com>
2017-09-25 20:41:43 +0800

    Add files via upload

---
+++ 237_Delete Node in a Linked List_easy.py
@@ -0,0 +1,18 @@
+'''237. Delete Node in a Linked List 
+   2017.9.25
+'''
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val=node.next.val
+        node.next=node.next.next
+        
