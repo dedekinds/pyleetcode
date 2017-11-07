fb05430603c380b2bf29daf5efd19ce671434885
dedekinds <469676727@qq.com>
2017-11-08 01:46:38 +0800

    Add files via upload

--- 46_Permutations_medium.py
+++ 46_Permutations_medium.py
@@ -247,3 +247,177 @@ class Solution(object):
                     new_res.append(r)
             res = new_res
         return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+_____________________________________________________
+C语言版STJ算法
+#include<stdio.h>
+#include<string.h>
+#define N 3
+
+int find_legal_move_loacation(int direction[], int nums[]){
+    int maxlocation=-10086;
+    int maxnum=-10086;
+    int x;
+    for(x=0;x<N;x++){
+        if(x+direction[x]>-1 && x+direction[x]<N){
+            if(nums[x]>nums[x+direction[x]] && nums[x]>=maxnum){
+                maxlocation=x;
+                maxnum=nums[x];
+            }
+        }
+    }
+    return maxlocation;
+}
+
+
+int main(){
+    int direction[N];
+    int nums[N];
+    int k;
+    int location;
+    int temp;
+    int a;
+    int temp_location;
+    int i;
+    for(i=0;i<N;i++)
+        nums[i]=i;
+    for(i=0;i<N;i++)
+        direction[i]=-1;
+        //={-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}
+
+    //k=find_legal_move_loacation(direction,nums);
+   //printf("%d ",k);
+    while(1){
+        int j;
+        for(j=0;j<N;j++){
+            printf("%d",nums[j]+1);
+        }
+        printf("\n");
+        location=find_legal_move_loacation(direction,nums);
+        //printf("%d",location);
+        if (location==-10086)
+            break;
+        //swapÊýÖµ
+            temp=nums[location];
+            nums[location]=nums[location+direction[location]];
+            nums[location+direction[location]]=temp;
+         //swap ·½Ïò
+            temp_location=location+direction[location];
+            a=direction[temp_location];
+            direction[temp_location]=direction[location];
+            direction[location]=a;
+         ///¸üÐÂ·½Ïò
+            int x;
+            for(x=0;x<N;x++){
+                if (nums[x]>temp){
+                    direction[x]*=-1;
+                }
+            }
+            //t+=1
+    }
+    return 0;
+}
+
+_____________________________________________________
+C语言版阶乘数算法
+#include<stdio.h>
+#include<string.h>
+#define N 3
+long factorial_iteration(int n){
+    int result = 1;
+
+    while(n>1){
+        result *= n;
+        n--;
+    }
+    return result;
+}
+
+int main(){
+    int Factorialnum[N];
+    int k;
+    int i;
+    int t;
+    int j;
+    int temp;
+    int tempnums[N];
+    int nums[N];
+    for(k=0;k<N;k++)
+        Factorialnum[k]=0;
+    int n=0;
+    for(i=0;i<N;i++)
+        nums[i]=i;
+    int times=1;
+    int total=factorial_iteration(N);
+
+
+    while(times<=total){
+
+     for(j=0;j<N;j++){
+            printf("%d",nums[j]+1);
+        }
+        printf("\n");
+
+      for(i=0;i<N;i++)
+        nums[i]=i;
+//生成下一个阶乘数（逆序数）__________________
+        n=0;
+
+       while(n<N){
+        if (Factorialnum[n]+1<N-n){
+            Factorialnum[n]+=1;
+            break;
+        }
+        else{
+            Factorialnum[n]=0;
+            n+=1;
+        }
+       }
+
+          //printf("%d",n);___________________________________
+
+
+
+      //逆序数→排列数______________________
+      i=0;
+    while(i<N-1){
+        if(Factorialnum[i]==0)
+            i+=1;
+        else{
+          for(j=i+Factorialnum[i];j>i;j--){
+                temp=nums[j];
+                nums[j]=nums[j-1];
+                nums[j-1]=temp;
+            }
+            //nums=nums[:i]+[t]+nums[i:i+Factorialnum[i]]+nums[1+i+Factorialnum[i]:]
+            i+=1;
+        }
+    }
+          //逆序数→排列数______________________
+
+
+
+        times+=1;
+    }
+    //逆序数→排列数
+
+
+    return 0;
+}
+
