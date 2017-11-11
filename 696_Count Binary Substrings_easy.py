7c5a21186bf6a22461d480f62b1bf6a447a2d64a
dedekinds <469676727@qq.com>
2017-11-11 23:49:13 +0800

    Add files via upload

--- 696_Count Binary Substrings_easy.py
+++ 696_Count Binary Substrings_easy.py
@@ -2,7 +2,24 @@
 696. Count Binary Substrings 
 2017.11.11
 '''
-
+#o(N)算法，http://bookshadow.com/weblog/2017/10/15/leetcode-count-binary-substrings/
+class Solution(object):
+    def countBinarySubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        size = len(s)
+        cnt = [0, 0]
+        last = None
+        ans = 0
+        for c in s:
+            c = int(c)
+            if c != last: cnt[c] = 0
+            cnt[c] += 1
+            if cnt[c] <= cnt[1 - c]: ans += 1#****!!!
+            last = c
+        return ans
 
 
 
