6d9b929274cdc575b8b31e2eda8f1e64531c36fc
dedekinds <469676727@qq.com>
2017-12-19 20:38:53 +0800

    Update 206_Reverse Linked List_easy.py

--- 206_Reverse Linked List_easy.py
+++ 206_Reverse Linked List_easy.py
@@ -21,3 +21,25 @@ class Solution:
             new.next=head
             head=temp
         return new.next
+
+    
+   ————————————————————————————————————————————
+递归的方法
+    # Definition for singly-linked list.  
+    # class ListNode(object):  
+    #     def __init__(self, x):  
+    #         self.val = x  
+    #         self.next = None  
+      
+    class Solution(object):  
+        def reverseList(self, head):  
+            """ 
+            :type head: ListNode 
+            :rtype: ListNode 
+            """  
+            if head is None or head.next is None:  
+                return head  
+            newHead = self.reverseList(head.next)  
+            head.next.next = head  
+            head.next = None  
+            return newHead  
