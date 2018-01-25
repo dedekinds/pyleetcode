38e2ccc42c44c3ba6e2112909895fb499834b807
dedekinds <469676727@qq.com>
2018-01-25 10:24:13 +0800

    Add files via upload

--- 647_Palindromic Substrings_medium.py
+++ 647_Palindromic Substrings_medium.py
@@ -2,6 +2,29 @@
 647. Palindromic Substrings
 2018.1.25
 '''
+用队列快很多（一倍的样子
+import collections
+class Solution(object):
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        #http://bookshadow.com/weblog/2017/07/24/leetcode-palindromic-substrings/
+        temp=collections.deque((x,x) for x in range(len(s)))
+        for x in range(len(s)-1):
+            if s[x]==s[x+1]:
+                temp.append((x,x+1))
+        
+        ans=0
+        while temp:
+            a,b=temp.popleft()
+            ans+=1#一开始单个字母
+            
+            if a-1>=0 and b+1<len(s) and s[a-1]==s[b+1]:
+                temp.append((a-1,b+1))
+        return ans
+_________________________________
 class Solution(object):
     def countSubstrings(self, s):
         """
