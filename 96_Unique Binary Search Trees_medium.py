f759eff9dc876d537d0c67188586e509a8f65442
dedekinds <469676727@qq.com>
2018-03-17 15:25:26 +0800

    Update 96_Unique Binary Search Trees_medium.py

--- 96_Unique Binary Search Trees_medium.py
+++ 96_Unique Binary Search Trees_medium.py
@@ -3,6 +3,17 @@
 '''
 
 结果是卡塔兰数：http://blog.csdn.net/u010582082/article/details/69569237?locationNum=7&fps=1
+考虑卡塔兰数的递推式：
+   ‘‘‘
+是左子树的情况+右子树的情况
+
+蔡某某 2018/3/17 15:22:24
+左子树有j个结点
+
+蔡某某 2018/3/17 15:22:35
+右子树有i-j-1个
+   ’’’
+
 import math
 class Solution(object):
     def numTrees(self, n):
