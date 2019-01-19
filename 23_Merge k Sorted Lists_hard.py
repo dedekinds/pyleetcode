6b5604e8dbda168091e823d4d529574da287d175
dedekinds <469676727@qq.com>
2019-01-19 17:07:59 +0800

    20190119

--- 23_Merge k Sorted Lists_hard.py
+++ 23_Merge k Sorted Lists_hard.py
@@ -26,3 +26,35 @@ class Solution:
                 Q.put((node.val,node))
         
         return res.next
+
+___________________________________
+O(kN)的做法
+class Solution:
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        res = ListNode(-1)
+        tmp = res
+        while True:
+            minv = float('inf')
+            minindex = -1
+            stop = True
+            
+            for i in range(len(lists)):
+                if lists[i]:
+                    #print(lists[i].val)
+                    stop = False
+                    if lists[i].val <= minv:
+                        minindex = i
+                        minv = lists[i].val
+            if stop:break
+            minnode = lists[minindex]
+            tmp.next = ListNode(minnode.val)
+            tmp = tmp.next
+            lists[minindex] = lists[minindex].next 
+            #minnode =minnode.next会错误，从列表中弹出数和赋值出来是不一样的
+               
+            
+        return res.next
